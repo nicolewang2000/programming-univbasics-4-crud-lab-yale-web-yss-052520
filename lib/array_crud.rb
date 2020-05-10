74dc23e8fefb75f3e100b71bfeeb795c596cddc3
Nicole Wang <nicole.wang@yale.edu>
2020-05-10 15:45:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ end
 
 def remove_element_from_end_of_array(array)
    array = ["I", "am", "really", "learning", "arrays!"]
-   
+   array.pop
+   ar
 end
 
 def remove_element_from_start_of_array(array)
